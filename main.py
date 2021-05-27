61df3061a6593c2dbd11ad3896ab7763bb1eac42
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2021-05-27 13:45:54 +0800

    demo usage of class RL

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from RL import ReplayMemory
-from IR import IR
+from RL import *
 
-rpmem = ReplayMemory()
-ir = IR()
+dict = {
+    'lr':0.01,
+    'gamma':0.9
+}
+my_framework = RL('src_ir',**{'lr':0.01,'gamma':0.9,'epsilon':0.9,'max_steps':30,'batch_size':128})
+my_framework.train()
 
