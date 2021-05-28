a6f451a8197ef551f495731ef7763e6e553c50d4
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2021-05-28 23:52:39 +0800

    update example usage for class RL

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from RL import *
 
-my_framework = RL('src_ir',**{'lr':0.01,'gamma':0.9,'epsilon':0.9,'max_steps':5,'batch_size':16})
+my_framework = RL('src_ir',**{'lr':0.01,'gamma':0.9,'epsilon':0.9,'max_steps':5,'batch_size':16,'eval_ratio':0.2})
 my_framework.train()
 
