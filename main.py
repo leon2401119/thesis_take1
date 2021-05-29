813ed1ff153f3ead04d4458949d3013bc1a1a821
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2021-05-29 17:06:42 +0800

    update example usage

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from RL import *
 
-my_framework = RL('src_ir',**{'lr':0.01,'gamma':0.9,'epsilon':0.9,'max_steps':5,'batch_size':16,'eval_ratio':0.2})
+my_framework = RL('src_ir',**{
+                    'lr': 0.01,
+                    'gamma': 0.9,
+                    'epsilon': 0.9,
+                    'max_steps': 15,
+                    'batch_size': 16,
+                    'eval_ratio': 0.2,
+                    'memsize': 10000
+                })
 my_framework.train()
 
